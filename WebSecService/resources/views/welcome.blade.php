4fd22a930d928809d1cac6bb81b073f85abe0a9e
Mahmoud Elsheikh <mahmoud230105572@sut.edu.eg>
2025-02-22 15:48:19 +0200

    Updating bootstrap files
    We updated the 2 bootstrap folders (js and css) to be bootstrap v5

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap JS (with Popper.js) -->
+    <script src="js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nemo et eligendi ipsam nesciunt fugiat natus autem cumque doloribus ratione quis, quae beatae, maiores perferendis placeat impedit fugit odio aliquam?</p>
