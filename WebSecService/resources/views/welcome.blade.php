6051941be273bd8519c543cd349b5b66044709c1
Mahmoud Elsheikh <mahmoud230105572@sut.edu.eg>
2025-02-22 19:05:47 +0200

    Prime number example
    here we mixed the Native PHP and Blade

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -8,19 +8,28 @@
     <script src="public/js/bootstrap.bundle.min.js"></script>
 </head>
 
-@php($j = 5)
+<?php
+    function isPrime($number) {
+        if($number<=1) return false;
+        $i = $number - 1;
+        while($i>1) {
+        if($number%$i==0) return false;
+            $i--;
+        }
+        return true;
+    }
+?>
 <body>
-    <div class="card">
-        <div class="card-header">Even Numbers</div>
+    <div class="card m-4">
+        <div class="card-header">Prime Numbers</div>
         <div class="card-body">
         @foreach (range(1, 100) as $i)
-        @if($i%2==0)
-            <span class="badge bg-primary">{{$i}}</span>  
+        @if(isPrime($i))
+        <span class="badge bg-primary">{{$i}}</span>  
         @else
-            <span class="badge bg-secondary">{{$i}}</span>  
+        <span class="badge bg-secondary">{{$i}}</span>  
         @endif
         @endforeach
     </div>
-</div>
-</body>
+</body> 
 </html>
