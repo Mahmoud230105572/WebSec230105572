144829b2b4fa1d3941830d80f1250ded0bd9c07e
Mahmoud Elsheikh <mahmoud230105572@sut.edu.eg>
2025-02-22 16:48:27 +0200

    Make Better design with bootstrap

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- Bootstrap CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Bootstrap JS (with Popper.js) -->
-    <script src="js/bootstrap.bundle.min.js"></script>
+    <link href="public/css/bootstrap.min.css" rel="stylesheet">
+    <script src="public/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nemo et eligendi ipsam nesciunt fugiat natus autem cumque doloribus ratione quis, quae beatae, maiores perferendis placeat impedit fugit odio aliquam?</p>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">New Card</div>
+            <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nemo et eligendi ipsam nesciunt fugiat natus autem cumque doloribus ratione quis, quae beatae, maiores perferendis placeat impedit fugit odio aliquam?</p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
