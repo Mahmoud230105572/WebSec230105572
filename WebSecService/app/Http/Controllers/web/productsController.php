62912a0d918d2c62d03bc33843c8ab610552194f
Mahmoud Elsheikh <mahmoud230105572@sut.edu.eg>
2025-03-05 21:08:08 +0200

    Adding the search bar to the products page

--- WebSecService/app/Http/Controllers/web/productsController.php
+++ WebSecService/app/Http/Controllers/web/productsController.php
@@ -8,15 +8,33 @@
 
     class ProductsController extends Controller{
     
-        public function list() {
-            // $products = Product::where("price",">",5000)->get(); // where
-            // $products = Product::where("code","like","tv%")->get(); //where (like)
-            // $products = Product::orderBy("price")->get(); // orderBy
-            // $products = Product::where("price",">",8000)->where("price","<",12000)->get(); // and
-            // $products = Product::where("price","=",1200)->orwhere("price","=",9000)->get(); // or
-            $products = Product::where("price", ">", 5000)
-                    ->orderBy("price", "desc")
-                    ->get();
+        public function list(Request $request) {
+            $query = Product::select("products.*");
+
+            // Filter by keywords (search in name)
+            $query->when($request->keywords, fn($q) => 
+                $q->where("name", "like", "%{$request->keywords}%")
+            );
+            
+            // Filter by min price
+            $query->when($request->min_price, fn($q) => 
+                $q->where("price", ">=", $request->min_price)
+            );
+            
+            // Filter by max price
+            $query->when($request->max_price, fn($q) => 
+                $q->where("price", "<=", $request->max_price)
+            );
+            
+            // Sorting (order by column)
+            $query->when($request->order_by, fn($q) => 
+                $q->orderBy($request->order_by, $request->order_direction ?? "ASC")
+            );
+            
+            // Get the filtered results
+            $products = $query->get();
+            
+            
 
 
             return view("products.list", compact('products'));
--- WebSecService/resources/views/products/list.blade.php
+++ WebSecService/resources/views/products/list.blade.php
@@ -3,39 +3,78 @@
 @section('content')
     
     <div class="container">
-        
-            <div class="row row-cols-3 g-y-2">
-                @foreach ($products as $product)
-                <div class="col">
-                    <div class="card">
-                        <div class="card-img-top"><img src="{{$product->photo}}" alt="{{$product->name}}" class="img-thumbnail"></div>
-                        <div class="card-body">
-                            <h2>{{$product->name}}</h2>
-                            <table class="table table-striped">
-                                <tr>
-                                    <td>Name</td>
-                                    <td>{{$product->name}}</td>
-                                </tr>
-                                <tr>
-                                    <td>Model</td>
-                                    <td>{{$product->model}}</td>
-                                </tr>
-                                <tr>
-                                    <td>Code</td>
-                                    <td>{{$product->code}}</td>
-                                </tr>
-                                <tr>
-                                    <td>Price</td>
-                                    <td style="color:rgb(41, 219, 41); font-weight:bold;">{{$product->price}}</td>
-                                </tr>
-                                <tr>
-                                    <td>Description</td>
-                                    <td>{{$product->description}}</td>
-                                </tr>
-                            </table>
+        <div class="card">
+            <div class="card-body">
+                <form>
+                    <div class="row">
+                        <div class="col col-sm-2">
+                            <input name="keywords" type="text"  class="form-control" placeholder="Search Keywords" value="{{ request()->keywords }}" />
+                        </div>
+                        <div class="col col-sm-2">
+                            <input name="min_price" type="numeric"  class="form-control" placeholder="Min Price" value="{{ request()->min_price }}"/>
+                        </div>
+                        <div class="col col-sm-2">
+                            <input name="max_price" type="numeric"  class="form-control" placeholder="Max Price" value="{{ request()->max_price }}"/>
+                        </div>
+                        <div class="col col-sm-2">
+                            <select name="order_by" class="form-select">
+                                <option value="" {{ request()->order_by==""?"selected":"" }} disabled>Order By</option>
+                                <option value="name" {{ request()->order_by=="name"?"selected":"" }}>Name</option>
+                                <option value="price" {{ request()->order_by=="price"?"selected":"" }}>Price</option>
+                            </select>
+                        </div>
+                        <div class="col col-sm-2">
+                            <select name="order_direction" class="form-select">
+                                <option value="" {{ request()->order_direction==""?"selected":"" }} disabled>Order Direction</option>
+                                <option value="ASC" {{ request()->order_direction=="ASC"?"selected":"" }}>ASC</option>
+                                <option value="DESC" {{ request()->order_direction=="DESC"?"selected":"" }}>DESC</option>
+                            </select>
+                        </div>
+                        <div class="col col-sm-1">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                        <div class="col col-sm-1">
+                            <button type="reset" class="btn btn-danger">Reset</button>
                         </div>
                     </div>
+                </form>
+            </div>
+        </div>
+
+
+
+        <div class="row row-cols-3 g-y-2">
+            @foreach ($products as $product)
+            <div class="col">
+                <div class="card">
+                    <div class="card-img-top"><img src="{{$product->photo}}" alt="{{$product->name}}" class="img-thumbnail"></div>
+                    <div class="card-body">
+                        <h2>{{$product->name}}</h2>
+                        <table class="table table-striped">
+                            <tr>
+                                <td>Name</td>
+                                <td>{{$product->name}}</td>
+                            </tr>
+                            <tr>
+                                <td>Model</td>
+                                <td>{{$product->model}}</td>
+                            </tr>
+                            <tr>
+                                <td>Code</td>
+                                <td>{{$product->code}}</td>
+                            </tr>
+                            <tr>
+                                <td>Price</td>
+                                <td style="color:rgb(41, 219, 41); font-weight:bold;">{{$product->price}}</td>
+                            </tr>
+                            <tr>
+                                <td>Description</td>
+                                <td>{{$product->description}}</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
+            </div>
 
                 @endforeach
             </div>
